59d1ba11e0fe4e84df39fbffc1df2c3a2077cfb0
Harshit Chaurasia <44460815+harshit0209@users.noreply.github.com>
2020-04-13 17:49:22 +0530

    done work

--- app/src/main/java/apps/theupbeats/noteslist/MainActivity.java
+++ app/src/main/java/apps/theupbeats/noteslist/MainActivity.java
@@ -1,16 +1,22 @@
 package apps.theupbeats.noteslist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Entity;
@@ -26,6 +32,7 @@ public class MainActivity extends ListActivity {
     Intent intent;
     Map<String, String> actionMap;
     DBHelper dbHelper;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,31 +41,47 @@ public class MainActivity extends ListActivity {
 
         initUI();
     }
+
     public void initUI()
     {
+
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitle("Lets Make a Note !");
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, "Menu", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
         dbHelper=new DBHelper(this);
         floatingActionButton=findViewById(R.id.floatingActionButton5);
          arrayList = new ArrayList<String>();
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
         setListAdapter(adapter);
         intent=new Intent(this,MakeNoteActivity.class);
-
         showActionMap();
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                intent.putExtra("show",false);
                 startActivityForResult(intent,2);
 
 
             }
         });
     }
+
+
     @Override
     protected void onListItemClick(ListView list, View view, int position, long id) {
         super.onListItemClick(list, view, position, id);
 
+        intent.putExtra("show",true);
+        intent.putExtra("data",actionMap.get(position));
+        startActivity(intent);
      //   String selectedItem = (String) getListView().getItemAtPosition(position);
         //String selectedItem = (String) getListAdapter().getItem(position);
 
--- app/src/main/java/apps/theupbeats/noteslist/MakeNoteActivity.java
+++ app/src/main/java/apps/theupbeats/noteslist/MakeNoteActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MakeNoteActivity extends AppCompatActivity {
@@ -17,24 +18,40 @@ EditText ti,co;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_note);
-        
+
+
+
         button=findViewById(R.id.button);
         ti=findViewById(R.id.editText2);
         co=findViewById(R.id.editText3);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String t=ti.getText().toString();
-                String c=co.getText().toString();
-                 boolean b=new DBHelper(getApplicationContext()).insertData(t,c);
-                 if(b)
-                makeResult(t);
-                 else
-                     Toast.makeText(MakeNoteActivity.this, "Error", Toast.LENGTH_SHORT).show();
-            }
-        });
-        
-        
+
+
+      if( getIntent().getBooleanExtra("show",false)) {
+
+          TextView tt=findViewById(R.id.textView);
+          tt.setText("Showing Note");
+          String temp[] = getIntent().getStringExtra("data").split(""+(char)99999);
+          ti.setText(temp[0]);
+          co.setText(temp[1]);
+          button.setVisibility(View.INVISIBLE);
+          ti.setEnabled(false);
+          co.setEnabled(false);
+      }
+      else {
+          button.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  String t = ti.getText().toString();
+                  String c = co.getText().toString();
+                  boolean b = new DBHelper(getApplicationContext()).insertData(t, c);
+                  if (b)
+                      makeResult(t);
+                  else
+                      Toast.makeText(MakeNoteActivity.this, "Error", Toast.LENGTH_SHORT).show();
+              }
+          });
+
+      }
     }
     void makeResult(String res)//call this to redirect the result to MAintActivity
     {
